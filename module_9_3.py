243a710ac5c8702d7f210e376a279c3dc7024ff7
RinkCoder29 <vyacheslav.vereshchagin17@gmail.com>
2024-12-06 02:42:39 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+om = zip(first, second)
+first_result = (abs(len(x) - len(y)) for x,y in zip(first,second)
+                if len(x) != len(y))
+
+second_result = (len(first[x]) == len(second[x]) for x in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
